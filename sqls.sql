2ac977e96538e5d9a2c365ac47efbe2cbcdb1a7b
afynr <ramacyber@gmail.com>
2016-08-08 01:04:22 +0700

    SQLs

--- sqls.sql
+++ sqls.sql
@@ -1,1 +1,85 @@
-ALTER TABLE `member` ADD UNIQUE(`member_id`);
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 07 Agu 2016 pada 20.04
+-- Versi Server: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `slims`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `biblio_rate`
+--
+
+CREATE TABLE `biblio_rate` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `biblio_id` int(10) UNSIGNED NOT NULL,
+  `member_id` varchar(20) NOT NULL DEFAULT '',
+  `rate` int(1) NOT NULL DEFAULT '1',
+  `input_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `member_logins`
+--
+
+CREATE TABLE `member_logins` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `member_id` varchar(20) NOT NULL,
+  `token` varchar(20) NOT NULL DEFAULT '',
+  `ip` varchar(20) NOT NULL DEFAULT '',
+  `browser` varchar(250) NOT NULL DEFAULT '',
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `biblio_rate`
+--
+ALTER TABLE `biblio_rate`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `member_logins`
+--
+ALTER TABLE `member_logins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `biblio_rate`
+--
+ALTER TABLE `biblio_rate`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `member_logins`
+--
+ALTER TABLE `member_logins`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
